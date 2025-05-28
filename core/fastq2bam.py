3bbac4ee35cfe4f611f3847dd8782a3d9345dbb9
yfan <fanyucai3@gmail.com>
2025-05-28 14:19:27 +0800

    no

--- core/fastq2bam.py
+++ core/fastq2bam.py
@@ -2,20 +2,26 @@ import os,sys,re
 import subprocess
 import argparse
 
+docker='virus:latest'
 # align reads with bowtie2 and sort bam with samtools
 def run(ref_index_dir,outdir,prefix,R1,R2=None):
     outdir=os.path.abspath(outdir)
-    if not os.path.exists(outdir):
-        subprocess.check_call(f'mkdir -p {outdir}',shell=True)
+    os.makedirs(outdir,exist_ok=True)
     out=outdir+"/"+prefix
     ref_index=""
     for i in os.listdir(ref_index_dir):
         if i.endswith(".rev.2.bt2"):
             ref_index = i.split(".rev.2.bt2")[0]
-    if R2 != None:
-        cmd= f"bowtie2 --threads 48 -x {ref_index_dir}/{ref_index} -1 {R1} -2 {R2}|samtools view -bh |samtools sort > {out}.bam && samtools index /outdir/{out}.bam"
+    R1=os.path.abspath(R1)
+    cmd=f'docker run --rm -v {R1}:/raw_data/{R1.split("/")[-1]} -v {os.path.abspath(ref_index_dir)}:/ref/ '
+    if R2:
+        cmd+=f"-v {R2}:/raw_data/{R2.split("/")[-1]} "
+    cmd+=f'{docker} sh -c \'export PATH=/opt/conda/bin/:$PATH && bowtie2 --threads 48 -x /ref/{ref_index} '
+
+    if R2:
+        cmd+="-1 {R1} -2 {R2}|samtools view -bh |samtools sort > {out}.bam && samtools index /outdir/{out}.bam\'"
     else:
-        cmd= f"bowtie2 --threads 48 -x {ref_index_dir}/{ref_index} -U {R1}|samtools view -bh |samtools sort > {out}.bam && samtools index {out}.bam"
+        cmd= f"-U {R1}|samtools view -bh |samtools sort > {out}.bam && samtools index {out}.bam\'"
     subprocess.check_call(cmd, shell=True)
 
 
