67d6db42d9bb37b270edadfc4548e6753bf1b9fc
yfan <fanyucai3@gmail.com>
2025-05-28 21:03:18 +0800

    no

--- pipeline.py
+++ pipeline.py
@@ -16,13 +16,14 @@ parser=argparse.ArgumentParser("Virus NGS pipeline\nEamile:fanyucai3@gmail.com")
 parser.add_argument("-p1","--pe1",help="R1 fastq",required=True, nargs='+')
 parser.add_argument("-p2","--pe2",help="R2 fastq",default=None,nargs='+')
 parser.add_argument("-p","--prefix",help="prefix of output",required=True, nargs='+')
-parser.add_argument("-b","--bed",help="bed file",default=None)
+parser.add_argument("-blastdb",'--blastdb',help="blast database name",required=True)
 parser.add_argument('-k','--kraken2',help='kraken2 reference index',required=True)
 parser.add_argument("-i","--identify",type=float,default=0.998)
-#parser.add_argument('-r','--ref',help="directory reference bowtie2 index",required=True)
-#parser.add_argument('-f','--fna',help="fasta reference",required=True)
 parser.add_argument("-host","--host",help="directory host bowtie2 index",required=True)
 parser.add_argument("-o","--outdir",help="diretory of output",required=True)
+parser.add_argument('-bowtie2','--bowtie2',help="directory reference bowtie2 index",default=None)
+parser.add_argument("-bed","--bed",help="bed file",default=None)
+parser.add_argument('-ref','--ref',help="reference fasta reference",default=None)
 args=parser.parse_args()
 
 args.outdir=os.path.abspath(args.outdir)
@@ -80,31 +81,54 @@ for r1,r2,prefix in zip(args.pe1,args.pe2,args.prefix):
                           f'cat spades_{prefix}/scaffolds.fasta megahit_{prefix}/{prefix}.contigs.fa >{prefix}.contigs.fa',shell=True)
     core.cd_hit_est.run(f'{args.outdir}/4.assembly/{prefix}.contigs.fa',args.identify,prefix+"non-redundant",f'{args.outdir}/4.assembly/')
 
+    ##########################################Step 5: blast NCBI Database: nt virus
     print("""
         # ------------------------
         # Step 5: blast NCBI Database: nt virus
         # ------------------------
     """)
+    core.blast.run(
+        f'{args.outdir}/4.assembly/{prefix}.non-redundant.fna',
+        args.blastdb,
+        f"{args.outdir}/5.blast/",
+        prefix,
+        10,
+    )
 
+    ##########################################step6:resequencing analysis
+    if args.bowtie2 and args.ref:
+        ##########################################Step 6-1: bowtie2 mapping reference
+        print("""
+            # ------------------------
+            # Step 6: bowtie2 mapping reference
+            # ------------------------
+        """)
 
-    # ------------------------
-    # Step 3: bowtie2 mapping reference
-    # ------------------------
+        if args.bed:
+            ##########################################Step 6-2: trim primer
+            print("""
+                # ------------------------
+                # Step 7: trim primer
+                # ------------------------
+            """)
 
+        ##########################################Step 6-3: variant calling and consensus sequence
+        print("""
+            # ------------------------
+            # Step 8: variant calling and consensus sequence
+            # ------------------------
+        """)
 
-      # ------------------------
-      # Step 4: trim primer
-      # ------------------------
+        ##########################################Step 6-4: plot coverage
+        print("""
+            # ------------------------
+            # Step 9: plot coverage
+            # ------------------------
+        """)
 
-  # ------------------------
-  # Step 5: variant calling and consensus sequence
-  # ------------------------
-
-  # ------------------------
-  # Step 6: plot coverage
-  # ------------------------
-
-
-  # ------------------------
-  # Step 7: run nextclade and pangolin
-  # ------------------------
+        ##########################################Step 6-5: run nextclade and pangolin
+        print("""
+            # ------------------------
+            # Step 10: run nextclade and pangolin
+            # ------------------------
+        """)
