c5d184136f898aa7a620e19e8113187f29e624de
yfan <fanyucai3@gmail.com>
2025-05-29 17:31:54 +0800

    no

--- core/trim_primer.py
+++ core/trim_primer.py
@@ -21,16 +21,16 @@ def run(bed,bam,outdir,prefix):
     ivar =cmd+ (f'ivar trim -e -i /raw_data/{bam.split("/")[-1]} -b /raw_data/{bed.split("/")[-1]} -p /outdir/{prefix}.soft.clipped '
             f'| tee /outdir/{prefix}.ivar.stdout && rm -rf /outdir/{prefix}.bam /outdir/{prefix}.bam.bai\'')
     print(ivar)
-    subprocess.check_call(ivar,shell=True)
+    #subprocess.check_call(ivar,shell=True)
 
     ## remove soft-clipped primers
     # https://jvarkit.readthedocs.io/en/latest/Biostar84452/
     # source activate && conda deactivate
     out_bam=cmd+ f"samtools sort /outdir/{prefix}.soft.clipped.bam -o /outdir/{prefix}.soft.clipped.sort.bam && jvarkit biostar84452 --samoutputformat BAM /outdir/{prefix}.soft.clipped.sort.bam |samtools sort -n >/outdir/{prefix}.trimmed.bam\'"
     print(out_bam)
-    subprocess.check_call(out_bam,shell=True)
+    #subprocess.check_call(out_bam,shell=True)
 
-    out_fastq=cmd+f"samtools fastq -1 /outdir/{prefix}_no_primer.R1.fq -2 /outdir/{prefix}_no_primer.R2.fq -s /outdir/{prefix}.singleton.fastq /outdir/{prefix}.trimmed.bam &>/outdir/{prefix}.bam2fastq.stdout\'"
+    out_fastq=cmd+f"samtools fastq -1 /outdir/{prefix}_no_primer.R1.fq -2 /outdir/{prefix}_no_primer.R2.fq -s /outdir/{prefix}.singleton.fastq /outdir/{prefix}.trimmed.bam\'"
     print(out_fastq)
 
     subprocess.check_call(out_fastq, shell=True)
@@ -43,3 +43,5 @@ if __name__=="__main__":
     parser.add_argument("-p","--prefix", help="prefix of output", required=True)
     args = parser.parse_args()
     run(args.bed, args.bam, args.outdir, args.prefix)
+
+3731247
