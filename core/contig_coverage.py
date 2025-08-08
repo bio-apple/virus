b72febd0a4d0f917e9a6822aa6e5b9bf8271b90f
yfan <fanyucai3@gmail.com>
2025-08-08 11:09:33 +0800

    No

--- core/contig_coverage.py
+++ core/contig_coverage.py
@@ -3,7 +3,7 @@ import argparse
 import subprocess
 
 docker='virus:latest'
-def run(ref,pe1,outdir,prefix,pe2=None):
+def run(ref,pe1,outdir,prefix,pe2=None,cov=5):
     ref=os.path.abspath(ref)
     pe1=os.path.abspath(pe1)
     pe2=os.path.abspath(pe2)
@@ -28,6 +28,16 @@ def run(ref,pe1,outdir,prefix,pe2=None):
                f'pileup.sh in=/outdir/{prefix}.sam.gz out=/outdir/{prefix}.cov.txt\'')
     print(cmd)
     subprocess.check_call(cmd, shell=True)
+    infile=open(f"{outdir}/{prefix}.cov.txt","r")
+    chr=[]
+    for line in infile:
+        line=line.strip()
+        if not line.startswith("#") and int(line.split("\t")[1]) >cov:
+            chr.append(line.split("\t")[1])
+    return chr
+
+
+
 
 
 if __name__=='__main__':
@@ -37,5 +47,6 @@ if __name__=='__main__':
     parser.add_argument("-p2","--pe2",help='PE2 fastq file',default=None)
     parser.add_argument("-o","--outdir",help='Output directory',default=os.getcwd())
     parser.add_argument("-p","--prefix",help='Output prefix',required=True)
+    parser.add_argument("-c","--coverage",help='min coverage threshold,default=5',default=5,type=int,required=True)
     args = parser.parse_args()
-    run(args.ref,args.pe1,args.outdir,args.prefix,args.pe2)
+    run(args.ref,args.pe1,args.outdir,args.prefix,args.pe2,args.coverage)
