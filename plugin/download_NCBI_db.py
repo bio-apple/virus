10d6e52e5ac0fb86e0c8830d1f394d4939ca019c
yfan <fanyucai3@gmail.com>
2025-08-13 14:32:20 +0800

    No

--- plugin/download_NCBI_db.py
+++ plugin/download_NCBI_db.py
@@ -1,25 +1,36 @@
 import subprocess
-import os
+import sys
+
 infile=open("download_ncbi_viruses.sh","w")
 
-for i in range (0,22):
-    if i<10:
-        infile.write(f"nohup wget https://ftp.ncbi.nlm.nih.gov/blast/db/nt_viruses.0{i}.tar.gz&\n"
-                 f"nohup wget https://ftp.ncbi.nlm.nih.gov/blast/db/nt_viruses.0{i}.tar.gz.md5&\n")
+def run(num):
+    for i in range (0,22):
+        if i<10:
+            infile.write(f"nohup wget https://ftp.ncbi.nlm.nih.gov/blast/db/nt_viruses.0{i}.tar.gz&\n"
+                     f"nohup wget https://ftp.ncbi.nlm.nih.gov/blast/db/nt_viruses.0{i}.tar.gz.md5&\n")
 
+        else:
+            infile.write(f"nohup wget https://ftp.ncbi.nlm.nih.gov/blast/db/nt_viruses.{i}.tar.gz&\n"
+                         f"nohup wget https://ftp.ncbi.nlm.nih.gov/blast/db/nt_viruses.{i}.tar.gz.md5&\n")
+    infile.close()
+    subprocess.check_call(f'bash download_ncbi_viruses.sh', shell=True)
+
+    for i in range(0,22):
+        if i<10:
+            subprocess.check_call(f'tar -xvf nt_viruses.0{i}.tar.gz',shell=True)
+
+        else:
+            subprocess.check_call(f'tar -xvf nt_viruses.{i}.tar.gz', shell=True)
+
+
+if __name__=="__main__":
+    if len(sys.argv)<2:
+        print(f"Usage: python {sys.argv[0]} <num>\n"
+              f"number files:nt_viruses.<num>.tar.gz\n"
+              f"https://ftp.ncbi.nlm.nih.gov/blast/db/")
     else:
-        infile.write(f"nohup wget https://ftp.ncbi.nlm.nih.gov/blast/db/nt_viruses.{i}.tar.gz&\n"
-                     f"nohup wget https://ftp.ncbi.nlm.nih.gov/blast/db/nt_viruses.{i}.tar.gz.md5&\n")
-
-# Download taxdb contains:taxdb.bti、taxdb.btd、taxonomy4blast.sqlite3
-infile.write("nohup wget https://ftp.ncbi.nlm.nih.gov/blast/db/taxdb.tar.gz &\n"
-             "nohup wget https://ftp.ncbi.nlm.nih.gov/blast/db/taxdb.tar.gz.md5&\n")
-infile.close()
-
-subprocess.check_call(f'bash download_ncbi_viruses.sh', shell=True)
-
-
-
+        num=int(sys.argv[1])
+        run(num)
 
 
 
