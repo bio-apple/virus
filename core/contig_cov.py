f109e2eb372eba818f1fd348690cbfb8a03927ae
yfan <fanyucai3@gmail.com>
2025-09-03 17:42:57 +0800

    No

--- core/contig_cov.py
+++ core/contig_cov.py
@@ -25,7 +25,7 @@ def run(ref,pe1,outdir,prefix,pe2=None,read_length=150):
                f'-v {ref}:/ref/{ref.split("/")[-1]} {docker} '
                f'sh -c \'export PATH=/opt/conda/bin:$PATH && '
                f'bbwrap.sh ref=/ref/{ref.split("/")[-1]} in=/raw_data/{pe1.split("/")[-1]} out=/outdir/{prefix}.sam.gz nodisk && '
-               f'pileup.sh in=/outdir/{prefix}.sam.gz out=/outdir/{prefix}.cov.txt nodisk \'')
+               f'pileup.sh in=/outdir/{prefix}.sam.gz out=/outdir/{prefix}.cov.txt\'')
     print(cmd)
     subprocess.check_call(cmd, shell=True)
     infile=open(f"{outdir}/{prefix}.cov.txt","r")
