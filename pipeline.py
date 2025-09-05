453802e5c8b74d2bf48ce78ec777012a809cf19a
yfan <fanyucai3@gmail.com>
2025-09-05 13:31:58 +0800

    No

--- pipeline.py
+++ pipeline.py
@@ -29,6 +29,9 @@ parser.add_argument("-p", "--prefix", help="prefix of output", required=True, na
 parser.add_argument("-o", "--outdir", help="diretory of output", required=True)
 parser.add_argument("-c", "--config", help="config file", required=True)
 parser.add_argument('-l', '--length', help="read length", type=int, required=True,choices=[50, 75, 100, 150, 200, 250, 300])
+parser.add_argument("-t", "--threads", help="number of threads to use", default=os.cpu_count(), type=int)
+parser.add_argument("-plot", "--plot", default=60, type=int, help="plot when Covered_percent >default=60")
+parser.add_argument("-fa", "--fa", default=70, type=int,help="output consensus fasta when Covered_percent >default=70")
 
 # Create a parameter group to organize the ref and bowtie2 parameters.
 ref_group = parser.add_argument_group("Reference/Bowtie2 Index Options")
@@ -190,7 +193,7 @@ for r1,r2,prefix in zip(args.pe1,args.pe2,args.prefix):
             # step8:variant calling,consensus sequence and plot coverage
             # ------------------------
             print("#------------------------\n#Step8:variant calling,consensus sequence and plot coverage\n#------------------------\n")
-            core.consensus.run(final_accession, read1, f'{args.outdir}/8.consensus/', prefix, nt_viruses, args.length,read2)
+            core.consensus.run(final_accession, read1, f'{args.outdir}/8.consensus/', prefix, nt_viruses, args.length,read2,args.plot,args.fa,args.threads)
             for key in final_accession:
                 subprocess.check_call(f'cd {args.outdir}/8.consensus/ && rm -rf {key}.bam {key}.bam.bai {key}.fa {key}.fasta {key}.*bt2 {key}.cov.txt',shell=True)
         else:
