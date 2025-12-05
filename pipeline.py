f22393ccd8ec41d8204a6c356dcd0df0b9d208ed
yfan <fanyucai3@gmail.com>
2025-12-05 15:16:38 +0800

    No

--- pipeline.py
+++ pipeline.py
@@ -107,7 +107,7 @@ for r1,r2,prefix in zip(args.pe1,args.pe2,args.prefix):
             count=core.merge_fastq.run(read1, read2, prefix, f'{args.outdir}/3.filter_host/', args.length)
             print(f"We merge {count} fastq files into {args.outdir}/3.filter_host/")
             if count!=0:
-                accession1 = core.blast.run(f'{args.outdir}/3.filter_host/{prefix}.non-redundant.fna', virus,f'{args.outdir}/3.filter_host/', prefix, 10, 98, 95, 1e-10, 1)
+                accession1 = core.blast.run(f'{args.outdir}/3.filter_host/{prefix}.non-redundant.fna', virus,f'{args.outdir}/3.filter_host/', prefix, 10, 98, 95, 1e-10, 5)
                 print(accession1)
     if args.ref and args.bowtie2:
         os.makedirs(f"{args.outdir}/4.vsp",exist_ok=True)
