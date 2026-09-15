927f186d3035c3e439a4320cbfb87fee26712b76
yfan <fanyucai3@gmail.com>
2026-09-15 15:10:39 +0800

    no

--- core/download_NCBI_db.py
+++ core/download_NCBI_db.py
@@ -1,40 +1,48 @@
 import subprocess
 import sys
 
-infile=open("download_ncbi_viruses.sh","w")
-
 def run(num):
-    for i in range (0,num):
-        if i<10:
-            infile.write(f"nohup wget https://ftp.ncbi.nlm.nih.gov/blast/db/nt_viruses.0{i}.tar.gz&\n"
-                     f"nohup wget https://ftp.ncbi.nlm.nih.gov/blast/db/nt_viruses.0{i}.tar.gz.md5&\n")
+    with open("download_ncbi_viruses.sh", "w") as infile:
+        infile.write("#!/bin/bash\n\n")
 
+        for i in range(num + 1):
+            if i < 10:
+                infile.write(
+                    f"wget https://ftp.ncbi.nlm.nih.gov/blast/db/nt_viruses.0{i}.tar.gz &\n"
+                )
+                infile.write(
+                    f"wget https://ftp.ncbi.nlm.nih.gov/blast/db/nt_viruses.0{i}.tar.gz.md5 &\n"
+                )
+            else:
+                infile.write(
+                    f"wget https://ftp.ncbi.nlm.nih.gov/blast/db/nt_viruses.{i}.tar.gz &\n"
+                )
+                infile.write(
+                    f"wget https://ftp.ncbi.nlm.nih.gov/blast/db/nt_viruses.{i}.tar.gz.md5 &\n"
+                )
+
+        # 等待所有 wget 完成
+        infile.write("\nwait\n")
+
+    # 执行下载脚本（会一直等到 wait 结束）
+    subprocess.check_call("bash download_ncbi_viruses.sh", shell=True)
+
+    # 下载完成后再解压
+    for i in range(num + 1):
+        if i < 10:
+            subprocess.check_call(f"tar -xvf nt_viruses.0{i}.tar.gz", shell=True)
         else:
-            infile.write(f"nohup wget https://ftp.ncbi.nlm.nih.gov/blast/db/nt_viruses.{i}.tar.gz&\n"
-                         f"nohup wget https://ftp.ncbi.nlm.nih.gov/blast/db/nt_viruses.{i}.tar.gz.md5&\n")
-    infile.close()
-    subprocess.check_call(f'bash download_ncbi_viruses.sh', shell=True)
-
-    for i in range(0,num):
-        if i<10:
-            subprocess.check_call(f'tar -xvf nt_viruses.0{i}.tar.gz',shell=True)
-        else:
-            subprocess.check_call(f'tar -xvf nt_viruses.{i}.tar.gz', shell=True)
+            subprocess.check_call(f"tar -xvf nt_viruses.{i}.tar.gz", shell=True)
 
 
-if __name__=="__main__":
-    if len(sys.argv)<2:
-        print(f"Usage: python {sys.argv[0]} <num>\n"
-              f"number files:nt_viruses.<num>.tar.gz\n"
-              f"https://ftp.ncbi.nlm.nih.gov/blast/db/")
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(
+            f"Usage: python {sys.argv[0]} <num>\n"
+            "number files: nt_viruses.<num>.tar.gz"
+        )
     else:
-        num=int(sys.argv[1])
-        run(num)
-
-
-
-
-
+        run(int(sys.argv[1]))
 
 
 
